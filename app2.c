860fd0c8a964dd02207567930c1eb62a59ee90e0
ryanfzy <ryanfzyw@gmail.com>
2016-12-01 21:46:16 +1300

    initaial commit for app2

---
+++ app2.c
@@ -0,0 +1,124 @@
+#include <windows.h>
+
+LRESULT CALLBACK MainWndProc(HWND, UINT, WPARAM, LPARAM);
+
+int STDCALL WinMain(HINSTANCE hInst, HINSTANCE hPrev, LPSTR lpCmd, int nShow)
+{
+    WNDCLASSEX wndClass;
+    memset(&wndClass, 0, sizeof(WNDCLASSEX));
+    wndClass.lpszClassName = "WinTestApp2Win";
+    wndClass.cbSize = sizeof(WNDCLASSEX);
+    wndClass.style = CS_HREDRAW | CS_VREDRAW;
+    wndClass.lpfnWndProc = MainWndProc;
+    wndClass.hInstance = hInst;
+    wndClass.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+    wndClass.hIconSm = LoadIcon(NULL, IDI_APPLICATION);
+    wndClass.hCursor = LoadCursor(NULL, IDC_ARROW);
+    wndClass.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);
+
+    RegisterClassEx(&wndClass);
+
+    HWND hWndMain = CreateWindow(
+        "WinTestApp2Win",
+        "Hello",
+        WS_OVERLAPPEDWINDOW,
+        CW_USEDEFAULT,
+        CW_USEDEFAULT,
+        CW_USEDEFAULT,
+        CW_USEDEFAULT,
+        NULL,
+        NULL,
+        hInst,
+        NULL
+    );
+
+    ShowWindow(hWndMain, nShow);
+    UpdateWindow(hWndMain);
+
+    MSG msg;
+    while (GetMessage(&msg, NULL, 0, 0))
+    {
+        TranslateMessage(&msg);
+        // IMPORTANT: this sends messages to MainWndProc
+        DispatchMessage(&msg);
+    }
+
+    return msg.wParam;
+}
+
+LRESULT CALLBACK MainWndProc(HWND hWnd, UINT nMsg, WPARAM wParam, LPARAM lParam)
+{
+    static HWND hWndButton = 0;
+    static int cx, cy;
+    HDC hdc;
+    PAINTSTRUCT ps;
+    RECT rc;
+
+    switch (nMsg)
+    {
+        case WM_CREATE:
+        {
+            TEXTMETRIC tm;
+            hdc = GetDC(hWnd);
+            SelectObject(hdc, GetStockObject(SYSTEM_FIXED_FONT));
+            GetTextMetrics(hdc, &tm);
+            cx = tm.tmAveCharWidth * 30;
+            cy = (tm.tmHeight + tm.tmExternalLeading) * 2;
+            ReleaseDC(hWnd, hdc);
+
+            hWndButton = CreateWindow(
+                "button",
+                "Click Here",
+                WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON,
+                0, 0,
+                cx, cy,
+                hWnd,
+                (HMENU)1,
+                ((LPCREATESTRUCT)lParam)->hInstance,
+                NULL
+            );
+            return 0;
+        }
+
+        case WM_DESTROY:
+        {
+            // IMPORTANT: this sends WM_QUIT to the message loop in WinMain, which will
+            // terminate the loop
+            PostQuitMessage(0);
+            return 0;
+        }
+
+        case WM_PAINT:
+        {
+            hdc = BeginPaint(hWnd, &ps);
+            GetClientRect(hWnd, &rc);
+            rc.bottom = rc.bottom/2;
+            DrawText(hdc, "Hello, World", -1, &rc, DT_SINGLELINE | DT_CENTER | DT_VCENTER);
+            EndPaint(hWnd, &ps);
+            return 0;
+        }
+
+        case WM_SIZE:
+        {
+            if (hWndButton && (wParam == SIZEFULLSCREEN || wParam == SIZENORMAL))
+            {
+                rc.left = (LOWORD(wParam) - cx) / 2;
+                rc.top = HIWORD(lParam) * 3 / 4 - cy / 2;
+                MoveWindow(hWndButton, rc. left, rc.top, cx, cy, TRUE);
+            }
+            break;
+        }
+
+        case WM_COMMAND:
+        {
+            if (LOWORD(wParam) == 1 && HIWORD(wParam) == BN_CLICKED && (HWND)lParam == hWndButton)
+            {
+                DestroyWindow(hWnd);
+            }
+            return 0;
+        }
+    }
+
+    return DefWindowProc(hWnd, nMsg, wParam, lParam);
+}
+
