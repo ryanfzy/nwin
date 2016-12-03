c377d46ced14538194bdbec22e0ca7f8a0d0e8ca
ryanfzy <ryanfzyw@gmail.com>
2016-12-03 23:29:53 +1300

    created some buttons

--- app2.c
+++ app2.c
@@ -1,6 +1,17 @@
 #include <windows.h>
+#include <stdio.h>
 
 LRESULT CALLBACK MainWndProc(HWND, UINT, WPARAM, LPARAM);
+static HWND CreateButton(HWND, LPCTSTR, int, int, int, int, HINSTANCE);
+
+static const int MainWindowWidth = 300;
+static const int MainWindowHeight = 400;
+
+static const int ButtonWidth = 50;
+static const int ButtonHeight = 50;
+static const int ButtonGap = 5;
+
+static const int MainWindowMargin = 10;
 
 int STDCALL WinMain(HINSTANCE hInst, HINSTANCE hPrev, LPSTR lpCmd, int nShow)
 {
@@ -22,10 +33,10 @@ int STDCALL WinMain(HINSTANCE hInst, HINSTANCE hPrev, LPSTR lpCmd, int nShow)
         "WinTestApp2Win",
         "Hello",
         WS_OVERLAPPEDWINDOW,
-        CW_USEDEFAULT,
-        CW_USEDEFAULT,
-        300,
-        400,
+        0,
+        0,
+        MainWindowWidth,
+        MainWindowHeight,
         NULL,
         NULL,
         hInst,
@@ -48,7 +59,7 @@ int STDCALL WinMain(HINSTANCE hInst, HINSTANCE hPrev, LPSTR lpCmd, int nShow)
 
 LRESULT CALLBACK MainWndProc(HWND hWnd, UINT nMsg, WPARAM wParam, LPARAM lParam)
 {
-    static HWND hWndButton = 0;
+    static HWND hWndButton[9];
     static int cx, cy;
     HDC hdc;
     PAINTSTRUCT ps;
@@ -58,6 +69,7 @@ LRESULT CALLBACK MainWndProc(HWND hWnd, UINT nMsg, WPARAM wParam, LPARAM lParam)
     {
         case WM_CREATE:
         {
+            /*
             TEXTMETRIC tm;
             hdc = GetDC(hWnd);
             SelectObject(hdc, GetStockObject(SYSTEM_FIXED_FONT));
@@ -65,18 +77,46 @@ LRESULT CALLBACK MainWndProc(HWND hWnd, UINT nMsg, WPARAM wParam, LPARAM lParam)
             cx = tm.tmAveCharWidth * 30;
             cy = (tm.tmHeight + tm.tmExternalLeading) * 2;
             ReleaseDC(hWnd, hdc);
+            */
 
+            // create number 1 to 9
+            int iButtonNum = 9;
+            int iButton = 0;
+            int iOffsetX, iOffsetY = MainWindowMargin;
+            for (int i = 0; i < 3; i++)
+            {
+                iOffsetX = MainWindowMargin;
+                for (int j = 0; j < 3; j++)
+                {
+                    char buttonName[2];
+                    sprintf(buttonName, "%d", iButtonNum);
+                    hWndButton[iButton] = CreateButton(hWnd, buttonName, iOffsetX, iOffsetY, ButtonWidth, ButtonHeight, ((LPCREATESTRUCT)lParam)->hInstance);
+                    iOffsetX = iOffsetX + ButtonWidth + ButtonGap;
+                    iButtonNum--;
+                }
+                iOffsetY = iOffsetY + ButtonHeight + ButtonGap;
+            }
+
+            iOffsetX = MainWindowMargin;
+            CreateButton(hWnd, "0", iOffsetX, iOffsetY, ButtonWidth*2+ButtonGap, ButtonHeight, ((LPCREATESTRUCT)lParam)->hInstance);
+            
+            iOffsetX = iOffsetX + ButtonWidth*2+ButtonGap + ButtonGap;
+            CreateButton(hWnd, ".", iOffsetX, iOffsetY, ButtonWidth, ButtonHeight, ((LPCREATESTRUCT)lParam)->hInstance);
+            /*
+            hWndButton = CreateButton(hWnd, 10, 10, ((LPCREATESTRUCT)lParam)->hInstance);
             hWndButton = CreateWindow(
-                "button",
-                "Click Here",
+                "BUTTON",
+                "1",
                 WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON,
-                0, 0,
-                cx, cy,
+                10, 10,
+                ButtonWidth,
+                ButtonHeight,
                 hWnd,
                 (HMENU)1,
                 ((LPCREATESTRUCT)lParam)->hInstance,
                 NULL
             );
+            */
             return 0;
         }
 
@@ -90,31 +130,38 @@ LRESULT CALLBACK MainWndProc(HWND hWnd, UINT nMsg, WPARAM wParam, LPARAM lParam)
 
         case WM_PAINT:
         {
+            /*
             hdc = BeginPaint(hWnd, &ps);
             GetClientRect(hWnd, &rc);
             rc.bottom = rc.bottom/2;
             DrawText(hdc, "Hello, World", -1, &rc, DT_SINGLELINE | DT_CENTER | DT_VCENTER);
             EndPaint(hWnd, &ps);
             return 0;
+            */
+            break;
         }
 
         case WM_SIZE:
         {
+            /*
             if (hWndButton && (wParam == SIZEFULLSCREEN || wParam == SIZENORMAL))
             {
                 rc.left = (LOWORD(wParam) - cx) / 2;
                 rc.top = HIWORD(lParam) * 3 / 4 - cy / 2;
                 MoveWindow(hWndButton, rc. left, rc.top, cx, cy, TRUE);
             }
+            */
             break;
         }
 
         case WM_COMMAND:
         {
+            /*
             if (LOWORD(wParam) == 1 && HIWORD(wParam) == BN_CLICKED && (HWND)lParam == hWndButton)
             {
                 DestroyWindow(hWnd);
             }
+            */
             return 0;
         }
     }
@@ -122,3 +169,21 @@ LRESULT CALLBACK MainWndProc(HWND hWnd, UINT nMsg, WPARAM wParam, LPARAM lParam)
     return DefWindowProc(hWnd, nMsg, wParam, lParam);
 }
 
+static HWND CreateButton(HWND hWnd, LPCTSTR lpName, int iOffsetX, int iOffsetY, int iWidth, int iHeight, HINSTANCE hInstance)
+{
+    HWND hWndButton = CreateWindow(
+        "BUTTON",
+        lpName,
+        WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON,
+        iOffsetX, 
+        iOffsetY,
+        iWidth,
+        iHeight,
+        hWnd,
+        (HMENU)1,
+        hInstance,
+        NULL
+    );
+
+    return hWndButton;
+}
