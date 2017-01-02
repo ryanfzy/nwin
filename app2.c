a7c957cd1217edb019ca8f5640a6b51c4143d71b
ryanfzy <ryanfzyw@gmail.com>
2017-01-02 22:28:15 +1300

    add a cal item info into a stack when press a button

--- app2.c
+++ app2.c
@@ -1,5 +1,6 @@
 #include <windows.h>
 #include <stdio.h>
+#include "lib/stack.h"
 
 LRESULT CALLBACK MainWndProc(HWND, UINT, WPARAM, LPARAM);
 static HWND CreateButton(HWND, LPCTSTR, int, int, int, int, int, HINSTANCE);
@@ -46,6 +47,21 @@ typedef struct tagCalButton
     int iColSpan;
 } CalButton;
 
+typedef enum
+{
+    CALITEMTYPE_NUM,
+    CALITEMTYPE_PLUS,
+    CALITEMTYPE_MINUS,
+    CALITEMTYPE_DIVIDE,
+    CALITEMTYPE_MULTIPLY,
+} CalItemType;
+
+typedef struct tagCalItemInfo
+{
+    long lnum;
+    CalItemType etype;
+} CalItemInfo;
+
 static const CalButton CalButtons[NUM_BUTTON_ROWS][NUM_BUTTON_COLS] = { 
     {
         {BUTTON_SEVEN, "7", 1, 1}, {BUTTON_EIGHT, "8", 1, 1}, {BUTTON_NINE, "9", 1, 1}, {BUTTON_DIVIDE, "/", 1, 1}, {BUTTON_PERCENTAGE, "%", 1, 1}
@@ -69,26 +85,33 @@ LRESULT CALLBACK CalScreenWndProc(HWND hWnd, UINT nMsg, WPARAM wParam, LPARAM lP
     RECT rcText1, rcText2;
     static char szText1[128] = { '\0' };
     static char szText2[128] = { '\0' };
-    static int iPosText2 = 0;
+    static int iText2Length = 0;
+    static Stack s;
 
     switch (nMsg)
     {
         case WM_CREATE:
+        {
+            stack_init(&s);
             return 0;
+        }
         case WM_PAINT:
         {
             hdc = BeginPaint(hWnd, &ps);
             Rectangle(hdc, ps.rcPaint.left, ps.rcPaint.top, ps.rcPaint.right, ps.rcPaint.bottom);
+
             rcText1.left = ps.rcPaint.left;
             rcText1.top = ps.rcPaint.top + 5;
             rcText1.right = ps.rcPaint.right - 5;
             rcText1.bottom = ps.rcPaint.bottom / 2;
+            DrawText(hdc, szText1, strlen(szText1), &rcText1, DT_VCENTER | DT_RIGHT);
+
             rcText2.left = ps.rcPaint.left;
             rcText2.top = ps.rcPaint.bottom / 2 + 5;
             rcText2.right = ps.rcPaint.right - 5;
             rcText2.bottom = ps.rcPaint.bottom;
-            DrawText(hdc, szText1, strlen(szText1), &rcText1, DT_VCENTER | DT_RIGHT);
             DrawText(hdc, szText2, strlen(szText2), &rcText2, DT_VCENTER | DT_RIGHT);
+
             EndPaint(hWnd, &ps);
             return 0;
         }
@@ -97,9 +120,50 @@ LRESULT CALLBACK CalScreenWndProc(HWND hWnd, UINT nMsg, WPARAM wParam, LPARAM lP
             int iButton = (int)lParam;
             if (iButton >= BUTTON_ZERO && iButton <= BUTTON_NINE)
             {
-                sprintf(szText2 + iPosText2, "%d", iButton - BUTTON_ZERO);
+                sprintf(szText2 + iText2Length, "%d", iButton - BUTTON_ZERO);
                 InvalidateRect(hWnd, NULL, TRUE);
-                iPosText2++;
+                iText2Length++;
+            }
+            else if (iButton == BUTTON_PLUS || iButton == BUTTON_MINUS ||
+                     iButton == BUTTON_DIVIDE || iButton == BUTTON_MULTIPLY)
+            {
+                CalItemInfo num, oper;
+                char *ptr;
+                char szOp[2];
+                num.lnum = strtol(szText2, &ptr, 10);
+                num.etype = CALITEMTYPE_NUM;
+                stack_push(&s, (char*)&num, sizeof(CalItemInfo));
+
+                if (iButton == BUTTON_PLUS)
+                {
+                    oper.etype = CALITEMTYPE_PLUS;
+                    strcpy(szOp, "+");
+                }
+                else if (iButton == BUTTON_MINUS)
+                {
+                    oper.etype = CALITEMTYPE_MINUS;
+                    strcpy(szOp, "-");
+                }
+                else if (iButton == BUTTON_DIVIDE)
+                {
+                    oper.etype = CALITEMTYPE_DIVIDE;
+                    strcpy(szOp, "/");
+                }
+                else if (iButton == BUTTON_MULTIPLY)
+                {
+                    oper.etype = CALITEMTYPE_MULTIPLY;
+                    strcpy(szOp, "*");
+                }
+
+                stack_push(&s, (char*)&oper, sizeof(CalItemInfo));
+
+                if (strlen(szText1) > 0)
+                    strcat(szText1, " ");
+                strcat(szText1, szText2);
+                strcat(szText1, " ");
+                strcat(szText1, szOp);
+                szText2[0] = '\0';
+                iText2Length = 0; 
             }
         }
         default:
@@ -247,14 +311,6 @@ LRESULT CALLBACK MainWndProc(HWND hWnd, UINT nMsg, WPARAM wParam, LPARAM lParam)
 
         case WM_PAINT:
         {
-            /*
-            hdc = BeginPaint(hWnd, &ps);
-            GetClientRect(hWnd, &rc);
-            rc.bottom = rc.bottom/2;
-            DrawText(hdc, "Hello, World", -1, &rc, DT_SINGLELINE | DT_CENTER | DT_VCENTER);
-            EndPaint(hWnd, &ps);
-            return 0;
-            */
             break;
         }
 
@@ -277,13 +333,14 @@ LRESULT CALLBACK MainWndProc(HWND hWnd, UINT nMsg, WPARAM wParam, LPARAM lParam)
             if (HIWORD(wParam) == BN_CLICKED)
             {
                 int iButtonId = LOWORD(wParam);
+                /*
                 if (iButtonId >= BUTTON_ZERO && iButtonId <= BUTTON_DOT)
                 {
                     char szMsg[2];
                     sprintf(szMsg, "%d", iButtonId - BUTTON_ZERO);
                     MessageBox(hWnd, szMsg, "Button", MB_OKCANCEL | MB_ICONINFORMATION);
-                    SendMessage(hWndCalScreen, WM_COMMAND, 0, iButtonId);
-                }
+                }*/
+                SendMessage(hWndCalScreen, WM_COMMAND, 0, iButtonId);
                 //DestroyWindow(hWnd);
             }
             return 0;
