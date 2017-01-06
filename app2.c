1a13b2b4079ebcc73fedf99a452e7fd0c727ae3a
ryanfzy <ryanfzyw@gmail.com>
2017-01-07 00:02:42 +1300

    removed calculation logic to a split file

--- app2.c
+++ app2.c
@@ -1,6 +1,7 @@
 #include <windows.h>
 #include <stdio.h>
 #include "lib/stack.h"
+#include "calculation.h"
 
 LRESULT CALLBACK MainWndProc(HWND, UINT, WPARAM, LPARAM);
 static HWND CreateButton(HWND, LPCTSTR, int, int, int, int, int, HINSTANCE);
@@ -48,21 +49,6 @@ typedef struct tagCalButton
     int iColSpan;
 } CalButton;
 
-typedef enum
-{
-    CALITEMTYPE_NUM,
-    CALITEMTYPE_PLUS,
-    CALITEMTYPE_MINUS,
-    CALITEMTYPE_DIVIDE,
-    CALITEMTYPE_MULTIPLY,
-} CalItemType;
-
-typedef struct tagCalItemInfo
-{
-    long lnum;
-    CalItemType etype;
-} CalItemInfo;
-
 static const CalButton CalButtons[NUM_BUTTON_ROWS][NUM_BUTTON_COLS] = { 
     {
         {BUTTON_CLEAR, "Clear", NUM_BUTTON_COLS, 1}, {BUTTON_NONE, "", 0, 0}, {BUTTON_NONE, "", 0, 0}, {BUTTON_NONE, "", 0, 0}, {BUTTON_NONE, "", 0, 0}
@@ -80,119 +66,6 @@ static const CalButton CalButtons[NUM_BUTTON_ROWS][NUM_BUTTON_COLS] = {
         {BUTTON_ZERO, "0", 2, 1}, {BUTTON_DOT, ".", 1, 1}, {BUTTON_NONE, "", 0, 0}, {BUTTON_PLUS, "+", 1, 1}, {BUTTON_NONE, "", 0, 0}
     }
 };
-
-// this only takes prefix expression
-static long do_get_result(Stack *ps)
-{
-    CalItemInfo sinfo;
-    stack_pop(ps, (char*)&sinfo, sizeof(CalItemInfo));
-    if (sinfo.etype == CALITEMTYPE_NUM)
-        return sinfo.lnum;
-    else
-    {
-        long num1;
-        long num2;
-
-        if (((CalItemInfo*)ps->ptop->pdata)->etype == CALITEMTYPE_NUM)
-        {
-            CalItemInfo sinfo2;
-            stack_pop(ps, (char*)&sinfo2, sizeof(CalItemInfo));
-            num1 = sinfo2.lnum;
-        }
-        else
-            num1 = do_get_result(ps);
-
-        if (((CalItemInfo*)ps->ptop->pdata)->etype == CALITEMTYPE_NUM)
-        {
-            CalItemInfo sinfo2;
-            stack_pop(ps, (char*)&sinfo2, sizeof(CalItemInfo));
-            num2 = sinfo2.lnum;
-        }
-        else
-            num2 = do_get_result(ps);
-
-        switch (sinfo.etype)
-        {
-            case CALITEMTYPE_PLUS:
-                return num1 + num2;
-            case CALITEMTYPE_MINUS:
-                return num1 - num2;
-            case CALITEMTYPE_MULTIPLY:
-                return num1 * num2;
-            case CALITEMTYPE_DIVIDE:
-                return num1 / num2;
-        }
-    }
-    return 0;
-}
-
-static long GetResult(Stack *ps, char *pret)
-{
-    long ret = 0;
-    if (ps->icount >0)
-    {
-        Stack s1, s2, s3;
-        stack_init(&s1);
-        stack_init(&s2);
-        stack_init(&s3);
-
-        CalItemInfo sinfo;
-        while (ps->icount > 0)
-        {
-            stack_pop(ps, (char*)&sinfo, sizeof(CalItemInfo));
-            if (sinfo.etype == CALITEMTYPE_NUM)
-            {
-                stack_push(&s1, (char*)&sinfo, sizeof(CalItemInfo));
-                stack_push(&s3, (char*)&sinfo, sizeof(CalItemInfo));
-            }
-            else if (sinfo.etype == CALITEMTYPE_PLUS || sinfo.etype == CALITEMTYPE_MINUS)
-            {
-                if (s2.icount > 0)
-                {
-                    CalItemInfo *psinfo = (CalItemInfo*)s2.ptop->pdata;
-                    while (psinfo != NULL && (psinfo->etype == CALITEMTYPE_MULTIPLY || psinfo->etype == CALITEMTYPE_DIVIDE))
-                    {
-                        CalItemInfo sinfo2;
-                        stack_pop(&s2, (char*)&sinfo2, sizeof(CalItemInfo));
-                        stack_push(&s1, (char*)&sinfo2, sizeof(CalItemInfo));
-                        stack_push(&s3, (char*)&sinfo2, sizeof(CalItemInfo));
-                        psinfo = s2.icount > 0 ? (CalItemInfo*)s2.ptop->pdata : NULL;
-                    }
-                }
-                stack_push(&s2, (char*)&sinfo, sizeof(CalItemInfo));
-            }
-            else if (sinfo.etype == CALITEMTYPE_MULTIPLY || sinfo.etype == CALITEMTYPE_DIVIDE)
-            {
-                stack_push(&s2, (char*)&sinfo, sizeof(CalItemInfo));
-            }
-        }
-
-        while (s2.icount > 0)
-        {
-            stack_pop(&s2, (char*)&sinfo, sizeof(CalItemInfo));
-            stack_push(&s1, (char*)&sinfo, sizeof(CalItemInfo));
-            stack_push(&s3, (char*)&sinfo, sizeof(CalItemInfo));
-        }
-
-        while (s1.icount > 0)
-        {
-            stack_pop(&s1, (char*)&sinfo, sizeof(CalItemInfo));
-            if (sinfo.etype == CALITEMTYPE_NUM)
-                sprintf(pret + strlen(pret), "%ld ", sinfo.lnum);
-            else if (sinfo.etype == CALITEMTYPE_PLUS)
-                strcat(pret, "+ ");
-            else if (sinfo.etype == CALITEMTYPE_MINUS)
-                strcat(pret, "- ");
-            else if (sinfo.etype == CALITEMTYPE_MULTIPLY)
-                strcat(pret, "* ");
-            else if (sinfo.etype == CALITEMTYPE_DIVIDE)
-                strcat(pret, "/ ");
-        }
-
-        ret = do_get_result(&s3);
-    }
-    return ret;
-}
 
 LRESULT CALLBACK CalScreenWndProc(HWND hWnd, UINT nMsg, WPARAM wParam, LPARAM lParam)
 {
@@ -294,7 +167,7 @@ LRESULT CALLBACK CalScreenWndProc(HWND hWnd, UINT nMsg, WPARAM wParam, LPARAM lP
                 stack_push(&s, (char*)&num, sizeof(CalItemInfo));
 
                 szText1[0] = '\0';
-                long ret = GetResult(&s, szText1);
+                long ret = calculate_result(&s, szText1);//GetResult(&s, szText1);
                 sprintf(szText2, "%ld", ret);
             }
             else if (iButton == BUTTON_CLEAR)
---
+++ calculation.h
@@ -0,0 +1,23 @@
+#ifndef CALCULATION_H
+#define CALCULATION_H
+
+#include "lib/stack.h"
+
+typedef enum
+{
+    CALITEMTYPE_NUM,
+    CALITEMTYPE_PLUS,
+    CALITEMTYPE_MINUS,
+    CALITEMTYPE_DIVIDE,
+    CALITEMTYPE_MULTIPLY,
+} CalItemType;
+
+typedef struct tagCalItemInfo
+{
+    long lnum;
+    CalItemType etype;
+} CalItemInfo;
+
+long calculate_result(Stack *ps, char *pret);
+
+#endif
